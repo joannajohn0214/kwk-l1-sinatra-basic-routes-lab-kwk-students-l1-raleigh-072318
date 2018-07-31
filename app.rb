91273a498777114f92d4facfd3421c6d7205dd0d
joannajohn0214 <joannajohn0214@gmail.com>
2018-07-31 15:20:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "Hello, World!"
+  end
+  get '/name' do 
+    "Hello, World!"
+  end
+  get '/name' do 
+    "Hello, World!"
+  end
 end
